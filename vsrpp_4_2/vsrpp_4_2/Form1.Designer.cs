fc94fadae3fb1810fb9628c452e00807373793a0
RomanHalaburda <mtbrider94@gmail.com>
2017-10-15 21:05:34 +0300

    design and events handlers done!

--- vsrpp_4_2/vsrpp_4_2/Form1.Designer.cs
+++ vsrpp_4_2/vsrpp_4_2/Form1.Designer.cs
@@ -29,22 +29,47 @@
         private void InitializeComponent()
         {
             this.dgv = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
             this.SuspendLayout();
             // 
             // dgv
             // 
             this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv.Location = new System.Drawing.Point(13, 13);
+            this.dgv.Location = new System.Drawing.Point(102, 12);
             this.dgv.Name = "dgv";
-            this.dgv.Size = new System.Drawing.Size(240, 150);
+            this.dgv.Size = new System.Drawing.Size(404, 200);
             this.dgv.TabIndex = 0;
+            this.dgv.CellBeginEdit += new System.Windows.Forms.DataGridViewCellCancelEventHandler(this.dgv_CellBeginEdit);
+            this.dgv.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_CellEndEdit);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add Row";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Generate .xls";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(842, 471);
+            this.ClientSize = new System.Drawing.Size(515, 219);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dgv);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +82,8 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- vsrpp_4_2/vsrpp_4_2/Form1.cs
+++ vsrpp_4_2/vsrpp_4_2/Form1.cs
@@ -18,9 +18,29 @@ namespace vsrpp_4_2
             InitializeComponent();
         }
 
+        DateTimePicker dtp;
+        String[] headerList = {"ФИО","Должность","Дата приёма","Стаж"};
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            dgv.ColumnCount = 4;
+            dgv.RowCount = 1;
+            dgv.RowHeadersVisible = false;
+            dgv.Width = 403;
+            dgv.Columns[0].Width = dgv.Columns[1].Width = dgv.Columns[2].Width = dgv.Columns[3].Width = 100;
 
+            for(int i = 0; i < dgv.ColumnCount; ++i)
+            {
+                dgv.Columns[i].HeaderCell.Value = headerList[i];            
+            }
+
+            dtp = new DateTimePicker();
+            dtp.Format = DateTimePickerFormat.Short;
+            dtp.Visible = false;
+            dtp.Width = 100;
+            dgv.Controls.Add(dtp);
+
+            dtp.ValueChanged += this.dtp_ValueChanged;
         }
 
         private void ExportToExcel()
@@ -35,24 +55,30 @@ namespace vsrpp_4_2
 
                 worksheet = workbook.ActiveSheet;
 
-                worksheet.Name = "ExportedFromDatGrid";
+                worksheet.Name = "Работники";
 
                 int cellRowIndex = 1;
                 int cellColumnIndex = 1;
-
+                
                 //Loop through each row and read value from each column. 
                 for (int i = 0; i < dgv.Rows.Count - 1; i++)
                 {
                     for (int j = 0; j < dgv.Columns.Count; j++)
                     {
-                        // Excel index starts from 1,1. As first Row would have the Column headers, adding a condition check. 
                         if (cellRowIndex == 1)
                         {
                             worksheet.Cells[cellRowIndex, cellColumnIndex] = dgv.Columns[j].HeaderText;
                         }
                         else
                         {
-                            worksheet.Cells[cellRowIndex, cellColumnIndex] = dgv.Rows[i].Cells[j].Value.ToString();
+                            if (dgv.Rows[i].Cells[j].Value != DBNull.Value)
+                            {
+                                worksheet.Cells[cellRowIndex, cellColumnIndex] = dgv.Rows[i].Cells[j].Value.ToString();
+                            }
+                            else
+                            {
+                                break;
+                            }
                         }
                         cellColumnIndex++;
                     }
@@ -81,6 +107,58 @@ namespace vsrpp_4_2
                 workbook = null;
                 excel = null;
             }
+        }
+
+        private void dgv_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        {
+            try
+            {
+                if(dgv.Focused && dgv.CurrentCell.ColumnIndex == 2)
+                {
+                    dtp.Location = dgv.GetCellDisplayRectangle(e.ColumnIndex, e.RowIndex, false).Location;
+                    dtp.Visible = true;
+                    /*
+                    if(dgv.CurrentCell.Value != DBNull.Value)
+                    {
+                        dtp.Value = (DateTime) dgv.CurrentCell.Value;
+                    }
+                    else
+                    {
+                        dtp.Value = DateTime.Today;
+                    }
+                    */
+                    dtp.Value = DateTime.Today;
+                }
+                else
+                {
+                    dtp.Visible = false;
+                }
+            }
+            catch(Exception ex)
+            { MessageBox.Show( ex.ToString()); }
+        }
+
+        private void dgv_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if(dgv.Focused && dgv.CurrentCell.ColumnIndex == 2)
+                {
+                    dgv.CurrentCell.Value = dtp.Value.Date;
+                }
+            }
+            catch(Exception ex)
+            { MessageBox.Show( ex.ToString()); }
+        }
+
+        private void dtp_ValueChanged(object sender, EventArgs e)
+        {
+            dgv.CurrentCell.Value = dtp.Value.Date;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dgv.RowCount++;
         }
     }
 }
