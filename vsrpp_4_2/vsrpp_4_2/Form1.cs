9827646c904d9642dea3680afc94a102bd7601dc
RomanHalaburda <mtbrider94@gmail.com>
2017-10-27 14:21:09 +0300

    Done!

--- vsrpp_4_2/vsrpp_4_2/Form1.cs
+++ vsrpp_4_2/vsrpp_4_2/Form1.cs
@@ -52,7 +52,6 @@ namespace vsrpp_4_2
 
             try
             {
-
                 worksheet = workbook.ActiveSheet;
 
                 worksheet.Name = "Работники";
@@ -88,13 +87,15 @@ namespace vsrpp_4_2
                     rowCount = i;
                 }
 
-                // Set format
+                // Set formatting
                 Range formatRange;
                 formatRange = worksheet.get_Range("a1", "d1");
                 formatRange.EntireRow.Font.Bold = true;
                 formatRange.Borders.Weight = XlBorderWeight.xlThin;
-                formatRange.HorizontalAlignment = HorizontalAlignment.Center;
 
+                worksheet.Range["A1", "D4"].Style.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+                worksheet.Range["A2","D" + rowCount].Style.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignLeft;
+                
                 rowCount++;
                 worksheet.get_Range("a2", "d" + rowCount).Borders.Weight = XlBorderWeight.xlMedium;
 
@@ -103,6 +104,21 @@ namespace vsrpp_4_2
                 ((Range)worksheet.Columns["C", System.Type.Missing]).EntireColumn.ColumnWidth = 25;
                 ((Range)worksheet.Columns["D", System.Type.Missing]).EntireColumn.ColumnWidth = 25;
 
+                // Biuld histogram chart
+                object misValue = System.Reflection.Missing.Value;
+
+                Microsoft.Office.Interop.Excel.Range chartRange;
+
+                Microsoft.Office.Interop.Excel.ChartObjects xlCharts = (Microsoft.Office.Interop.Excel.ChartObjects)worksheet.ChartObjects(Type.Missing);
+                Microsoft.Office.Interop.Excel.ChartObject myChart = (Microsoft.Office.Interop.Excel.ChartObject)xlCharts.Add(10, 80, 300, 250);
+                Microsoft.Office.Interop.Excel.Chart chartPage = myChart.Chart;
+
+                Range fCol = worksheet.get_Range("A2", "A" + rowCount);
+                Range sCol = worksheet.get_Range("D2", "D" + rowCount);
+                chartRange = worksheet.get_Range(fCol, sCol);
+                chartPage.SetSourceData(chartRange, misValue);
+                chartPage.ChartType = Microsoft.Office.Interop.Excel.XlChartType.xlColumnClustered;
+                               
                 //Getting the location and file name of the excel to save from user. 
                 SaveFileDialog saveDialog = new SaveFileDialog();
                 saveDialog.Filter = "Excel files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
@@ -162,8 +178,8 @@ namespace vsrpp_4_2
                 if ((dgv.Focused) && (dgv.CurrentCell.ColumnIndex == 2))
                 {
                     dgv.CurrentCell.Value = dtp.Value.Date.Date;
-                    dgv.Rows[dgv.CurrentCell.RowIndex].Cells[3].Value =
-                        DateTime.Today.Date - (DateTime)dgv.CurrentCell.Value;
+                    uint delta = Convert.ToUInt32((DateTime.Today.Date - (DateTime)dgv.CurrentCell.Value).Days);
+                    dgv.Rows[dgv.CurrentCell.RowIndex].Cells[3].Value = delta;
                 }
             }
             catch (Exception ex)
@@ -185,8 +201,22 @@ namespace vsrpp_4_2
             ExportToExcel();
         }
 
-        private void setFormat()
+        private void releaseObject(object obj)
         {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Exception Occured while releasing object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
         }
     }
 }
